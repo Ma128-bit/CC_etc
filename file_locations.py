5d8ee1b275a3fdd662c76855d96b7a43109209ef
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-29 17:02:15 +0100

    Update file_locations.py

--- file_locations.py
+++ file_locations.py
@@ -1,1 +1,24 @@
-sss
+data_path = "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/"
+    
+tau3mu_Run2022C = data_path + "Era_C_tau3mu.root"
+tau3mu_Run2022D = data_path + "Era_D_tau3mu.root"
+tau3mu_Run2022E = data_path + "Era_E_tau3mu.root"
+tau3mu_Run2022F = data_path + "Era_F_tau3mu.root"
+tau3mu_Run2022G = data_path + "Era_G_tau3mu.root"
+
+control_Run2022C = data_path + "Era_C_control.root"
+control_Run2022D = data_path + "Era_D_control.root"
+control_Run2022E = data_path + "Era_E_control.root"
+control_Run2022F = data_path + "Era_F_control.root"
+control_Run2022G = data_path + "Era_G_control.root"
+
+MC2022_B0_pre = data_path + "MC_B0_preE.root"
+MC2022_B0_post = data_path + "MC_B0_postE.root"
+MC2022_Bp_pre = data_path + "MC_Bp_preE.root"
+MC2022_Bp_post = data_path + "MC_Bp_postE.root"
+MC2022_Ds_pre = data_path + "MC_Ds_preE.root"
+MC2022_Ds_post = data_path + "MC_Ds_postE.root"
+
+MC2022_DsPhiPi_pre = data_path + "MC_DsPhiPi_preE.root"
+MC2022_DsPhiPi_post = data_path + "MC_DsPhiPi_postE.root"
+
