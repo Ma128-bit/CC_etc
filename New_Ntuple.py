2148a725f9789401f0d833fa4d72610ce050fce3
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-04 15:15:52 +0100

    Update New_Ntuple.py

--- New_Ntuple.py
+++ New_Ntuple.py
@@ -1,2 +1,14 @@
+import sys, os, subprocess, json
+from datetime import datetime
+import numpy as np
+import pandas as pd
 import uproot
 from ROOT import *
+def load_data(self, file_name):
+    """Load ROOT data and turn tree into a pd dataframe"""
+    print("Loading data from", file_name)
+    f = uproot.open(file_name)
+    tree = f[self.tree_name]
+    branches = self.all_branches
+    data = tree.arrays(branches ,library="pd")
+    return data
