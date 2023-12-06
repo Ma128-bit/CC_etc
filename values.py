14c8b515ad5a5e140e24e7b800032b38c79d01e3
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-06 12:14:22 +0100

    Create values.py

---
+++ values.py
@@ -0,0 +1,35 @@
+xsection_Bp_preE = 3.508e+9
+xsection_Bp_postE = 3.538e+9
+xsection_Ds_preE = 9.827e+9
+xsection_Ds_postE = 9.815e+9
+xsection_B0_preE = 3.520e+9
+xsection_B0_postE = 3.525e+9
+xsection_DsPhiPi_preE = 1.106e+10
+xsection_DsPhiPi_postE = 1.103e+10
+
+lumi_tau3mu_preE = 8.052
+lumi_tau3mu_postE = 26.758
+lumi_control_preE = 0.397
+lumi_control_postE = 1.33
+
+BR_tau3mu = 1.0e-7
+BR_control = 1.29e-5
+
+BR_Dstau = 5.48e-2
+BR_DsPhiPi = 1.3e-5
+BR_Bptau = 3.33e-2
+BR_B0tau = 3.35e-2
+
+N_Bp_preE = 515160
+N_Bp_postE = 1627733
+N_Ds_preE = 2077873
+N_Ds_postE = 7428463
+N_B0_preE = 837468
+N_B0_postE = 2702174
+N_DsPhiPi_preE = 297926
+N_DsPhiPi_postE = 1199059
+
+weight_CC_preE = 0.77
+weight_CC_postE = 1.03
+weight_CC_preE_err = 0.09
+weight_CC_postE_err = 0.05
