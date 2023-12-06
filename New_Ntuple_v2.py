ef2d78628f6d54393dbe5bace3766bc1b552cf2a
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-06 14:45:05 +0100

    Update New_Ntuple_v2.py

--- New_Ntuple_v2.py
+++ New_Ntuple_v2.py
@@ -26,50 +26,51 @@ branches_tau3mu =[
 ]
 
 Files = {
-    "C_tau3mu": [tau3mu_Run2022C, 0],
-    "D_tau3mu": [tau3mu_Run2022D, 0],
-    "E_tau3mu": [tau3mu_Run2022E, 0],
-    "F_tau3mu": [tau3mu_Run2022F, 0],
-    "G_tau3mu": [tau3mu_Run2022G, 0],
-    "B0_preE_tau3mu": [MC2022_B0_pre, 1],
-    "B0_postE_tau3mu": [MC2022_B0_post, 1],
-    "Bp_preE_tau3mu": [MC2022_Bp_pre, 2],
-    "Bp_postE_tau3mu": [MC2022_Bp_post, 2],
-    "Ds_preE_tau3mu": [MC2022_Ds_pre, 3],
-    "Ds_postE_tau3mu": [MC2022_Ds_post, 3],
-    "C_control": [control_Run2022C, 0],
-    "D_control": [control_Run2022D, 0],
-    "E_control": [control_Run2022E, 0],
-    "F_control": [control_Run2022F, 0],
-    "G_control": [control_Run2022G, 0],
-    "DsPhiPi_preE_control": [MC2022_DsPhiPi_pre, 4],
-    "DsPhiPi_postE_control": [MC2022_DsPhiPi_post, 4]
+    "tau3mu": [tau3mu_Run2022C, tau3mu_Run2022D, tau3mu_Run2022E, tau3mu_Run2022F, tau3mu_Run2022G, MC2022_B0_pre, MC2022_B0_post, MC2022_Bp_pre, MC2022_Bp_post, MC2022_Ds_pre, MC2022_Ds_post],
+    "control": [control_Run2022C, control_Run2022D, control_Run2022E, control_Run2022F, control_Run2022G, MC2022_DsPhiPi_pre, MC2022_DsPhiPi_post]
 }
 
 def load_df(isTau3mu, treename):
     if isTau3mu == True:
-        string = "tau3mu"
+        files = Files["tau3mu"]
         br = branches+branches_tau3mu
     else:
-        string = "control"
+        files = Files["control"]
         br = branches
-    files = []
-    for key, value in Files.items():
-        if string in key:
-            files.append(value[0])
     frame = RDataFrame(treename, files, br)
     return frame
 
 ROOT.gInterpreter.Declare(
 """
-int MCLable(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
+int add_weight(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
+    double xsection_Bp_preE = 3.508e+9, xsection_Bp_postE = 3.538e+9;
+    double xsection_Ds_preE = 9.827e+9, xsection_Ds_postE = 9.815e+9;
+    double xsection_B0_preE = 3.520e+9, xsection_B0_postE = 3.525e+9;
+    double xsection_DsPhiPi_preE = 1.106e+10, xsection_DsPhiPi_postE = 1.103e+10;
+
+    double lumi_tau3mu_preE = 8.052, lumi_tau3mu_postE = 26.758;
+    double lumi_control_preE = 0.397, lumi_control_postE = 1.33;
+
+    double BR_tau3mu = 1.0e-7, BR_control = 1.29e-5;
+
+    double BR_Dstau = 5.48e-2, BR_DsPhiPi = 1.3e-5;
+    double BR_Bptau = 3.33e-2, BR_B0tau = 3.35e-2;
+
+    int N_Bp_preE = 515160, N_Bp_postE = 1627733;
+    int N_Ds_preE = 2077873, N_Ds_postE = 7428463;
+    int N_B0_preE = 837468, N_B0_postE = 2702174;
+    int N_DsPhiPi_preE = 297926, N_DsPhiPi_postE = 1199059;
+
     std::cout<<"id: "<<id.AsString()<<std::endl;
-    if(id.Contains("Era_")) return 0;
-    else if(id.Contains("MC_B0")) return 1;
-    else if(id.Contains("MC_Bp")) return 2;
-    else if(id.Contains("MC_Ds_")) return 3;
-    else if(id.Contains("MC_DsPhiPi")) return 4;
-    else return -1;
+    else if(id.Contains("MC_Ds_preE.root")) return (xsection_Ds_preE*lumi_tau3mu_preE*BR_tau3mu*BR_Dstau/N_Ds_preE);
+    else if(id.Contains("MC_Ds_postE.root")) return (xsection_Ds_postE*lumi_tau3mu_postE*BR_tau3mu*BR_Dstau/N_Ds_postE);
+    else if(id.Contains("MC_B0_preE.root")) return (xsection_B0_preE*lumi_tau3mu_preE*BR_tau3mu*BR_B0tau/N_B0_preE);
+    else if(id.Contains("MC_B0_postE.root")) return (xsection_B0_postE*lumi_tau3mu_postE*BR_tau3mu*BR_B0tau/N_B0_postE);
+    else if(id.Contains("MC_Bp_preE.root")) return (xsection_Bp_preE*lumi_tau3mu_preE*BR_tau3mu*BR_Bptau/N_Bp_preE);
+    else if(id.Contains("MC_Bp_postE.root")) return (xsection_Bp_postE*lumi_tau3mu_postE*BR_tau3mu*BR_Bptau/N_Bp_postE);
+    else if(id.Contains("MC_DsPhiPi_preE.root")) return (xsection_DsPhiPi_preE*lumi_control_preE*BR_control*BR_DsPhiPi/N_DsPhiPi_preE);
+    else if(id.Contains("MC_DsPhiPi_postE.root")) return (xsection_DsPhiPi_postE*lumi_control_postE*BR_control*BR_DsPhiPi/N_DsPhiPi_postE);
+    else return 1;
 }
 """)
 
