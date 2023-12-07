6818e5c6c7162b15c88d4516f786f3034991b4c2
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-07 16:17:55 +0100

    Update cpp_library.h

--- cpp_library.h
+++ cpp_library.h
@@ -26,10 +26,10 @@ double BR_tau3mu = 1.0e-7, BR_control = 1.29e-5;
 double BR_Dstau = 5.48e-2, BR_DsPhiPi = 1.3e-5;
 double BR_Bptau = 3.33e-2, BR_B0tau = 3.35e-2;
 
-int N_Bp_preE = 515160, N_Bp_postE = 1627733;
-int N_Ds_preE = 2077873, N_Ds_postE = 7428463;
-int N_B0_preE = 837468, N_B0_postE = 2702174;
-int N_DsPhiPi_preE = 297926, N_DsPhiPi_postE = 1199059;
+double N_Bp_preE = 515160.0, N_Bp_postE = 1627733.0;
+double N_Ds_preE = 2077873.0, N_Ds_postE = 7428463.0;
+double N_B0_preE = 837468.0, N_B0_postE = 2702174.0;
+double N_DsPhiPi_preE = 297926.0, N_DsPhiPi_postE = 1199059.0;
 
 double weight_CC_preE = 0.77, weight_CC_postE = 1.03;
 double weight_CC_preE_err = 0.09, weight_CC_postE_err = 0.05;
