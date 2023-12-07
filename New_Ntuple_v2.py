44ce5e4f9393329afa660b8cd6e07bd582c77863
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-07 11:35:50 +0100

    Update New_Ntuple_v2.py

--- New_Ntuple_v2.py
+++ New_Ntuple_v2.py
@@ -44,7 +44,7 @@ def load_df(isTau3mu, treename):
 ROOT.gInterpreter.Declare(
 """
 """)
-ROOT.gSystem.Load("cpp_library.cpp")
+ROOT.gSystem.Load("cpp_library.so")
 
 if __name__ == "__main__":
     df = load_df(True, "FinalTree")
