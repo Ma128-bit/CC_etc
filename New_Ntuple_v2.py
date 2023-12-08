874d74af07407b23144ca3d49fa5b1ccec1696b6
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-08 21:28:51 +0100

    Update New_Ntuple_v2.py

--- New_Ntuple_v2.py
+++ New_Ntuple_v2.py
@@ -72,10 +72,10 @@ if __name__ == "__main__":
         df = df.Define("Muon3_SF_err", ROOT.SF_WeightsComputer(SF_pre, SF_post, True), ["ID", "Ptmu3", "Etamu3"])
 
     histo_file = TFile.Open(PV_SFs)
-    h_vectors = ROOT.std.vector(ROOT.std.pair(ROOT.TString, ROOT.TH1F))()
+    h_vectors = ROOT.std.vector(ROOT.std.tuple(ROOT.TString, ROOT.TH1F))()
     h_names = ["B0_preE", "B0_postE", "Bp_preE", "Bp_postE", "Ds_preE", "Ds_postE", "DsPhiPi_preE", "DsPhiPi_postE"]
     for key in h_names:
-        h_vectors.push_back(ROOT.std.make_pair(key, histo_file.Get("ratio_h_" + key)))
+        h_vectors.push_back(ROOT.std.make_tuple(key, histo_file.Get("ratio_h_" + key)))
 
     print(h_vectors)
     
