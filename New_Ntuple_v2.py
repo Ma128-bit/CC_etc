063d8c895a28c8c4b1358c39fc57918e876f1c1b
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-08 21:26:41 +0100

    Update New_Ntuple_v2.py

--- New_Ntuple_v2.py
+++ New_Ntuple_v2.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     h_vectors = std.vector(std.pair(ROOT.TString, ROOT.TH1F))()
     h_names = ["B0_preE", "B0_postE", "Bp_preE", "Bp_postE", "Ds_preE", "Ds_postE", "DsPhiPi_preE", "DsPhiPi_postE"]
     for key in h_names:
-        h_vectors.push_back(std.make_pair(key, histo_file.Get("ratio_h_" + key)));
+        h_vectors.push_back(std.make_pair(key, histo_file.Get("ratio_h_" + key)))
 
     print(h_vectors)
     
