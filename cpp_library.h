6a40978690c217473725103f40c44dd6520fe617
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-09 18:11:05 +0100

    Update cpp_library.h

--- cpp_library.h
+++ cpp_library.h
@@ -114,9 +114,7 @@ struct SF_WeightsComputer{
 struct PV_WeightsComputer{
     std::vector<TString> name;
     std::vector<TH1F*> histo;
-    std::map<TString, TH1F*> map;
     bool flag;
-    //PV_WeightsComputer(std::vector<TString>& s, const std::vector<TH1F*>& histograms, const std::map<TString, TH1F*> &m, bool f): name(s), histo(histograms), map(m), flag(f) {}
     PV_WeightsComputer(std::vector<TString>& s, const std::vector<TH1F*>& histograms, bool f): name(s), histo(histograms), flag(f) {}
     float operator()(const TString& ID, const double nVtx) {
         auto it = std::find(name.begin(), name.end(), ID);
