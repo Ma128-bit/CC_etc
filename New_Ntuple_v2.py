5f4b66313b0f4573803677554d3b0fa7d8a36318
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-09 17:50:35 +0100

    Update New_Ntuple_v2.py

--- New_Ntuple_v2.py
+++ New_Ntuple_v2.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
         h_map[h_name[j]]=histo_file.Get("ratio_h_" + key)
         j=j+1
     
-    df = df.Define("weight_nVtx", ROOT.PV_WeightsComputer(h_name, h_vectors, h_map, False), ["ID", "nVtx"])
+    df = df.Define("weight_nVtx", ROOT.PV_WeightsComputer(h_name, h_vectors, False), ["ID", "nVtx"])
     #df = df.Define("weight_nVtx_err", ROOT.PV_WeightsComputer(h_name, h_vectors, True), ["ID", "nVtx"])
     
     df.Snapshot("FinalTree", "AllData.root")
