2deffc30b5d302b271c48743af1a68e6038aa78f
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-11 18:49:43 +0100

    Update New_Ntuple.py

--- New_Ntuple.py
+++ New_Ntuple.py
@@ -100,14 +100,16 @@ if __name__ == "__main__":
 
     if not os.path.exists("ROOTFiles"):
         subprocess.run(["mkdir", "ROOTFiles"])
-        
+
     if isTau3mu==True:
+        b_weights = ["weight", "weight_MC", "weight_CC","Muon3_SF","Muon2_SF","Muon1_SF","Muon3_SF_err","Muon2_SF_err","Muon1_SF_err","weight_nVtx", "weight_nVtx_err", "training_weight", "combine_weight"]
         df = df.Define("training_weight", "weight_MC * weight_CC * Muon3_SF * weight_nVtx")
         df = df.Define("combine_weight", "weight * weight_CC * Muon3_SF * weight_nVtx")
-        df.Snapshot("FinalTree", "ROOTFiles/AllData.root", branches+branches_tau3mu)
+        df.Snapshot("FinalTree", "ROOTFiles/AllData.root", branches+branches_tau3mu+b_weights)
     else:
+        b_weights = ["weight","Muon1_SF","Muon2_SF","Muon1_SF_err","Muon2_SF_err","weight_nVtx", "weight_nVtx_err", "control_weight"]
         df = df.Define("control_weight", "weight * weight_nVtx")
-        df.Snapshot("FinalTree", "ROOTFiles/AllControl.root", branches)
+        df.Snapshot("FinalTree", "ROOTFiles/AllControl.root", branches+b_weights)
     
     print("Performed ",df.GetNRuns()," loops")
     end = time.time()
