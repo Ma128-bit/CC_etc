9f3e734237b23667105e459539c08a60052ea761
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-12 19:32:04 +0100

    Update fitDimu_OS.cpp

--- fitDimu_OS.cpp
+++ fitDimu_OS.cpp
@@ -8,20 +8,24 @@
 #include <RooGaussian.h>
 #include <RooFitResult.h>
 
+using namespace RooFit
+
 void single_fit(TString cat, TString lable, TH1F* histo){
     // Crea una variabile RooRealVar per dimu_OS1
-    RooRealVar x("dimu_OS1", "Dimu_OS1", 0.96, 1.06);
+    RooRealVar x("dimu_OS1", "Dimu_OS1",  0.90, 1.10);
+    int nBins = histo->GetNbinsX();
+    x.setBins(nBins);
 
     // Crea un RooDataHist dal nostro istogramma
-    RooDataHist data("data", "Data from Tree", x, RooFit::Import(*histo));
-
+    RooDataHist data("data", histo->GetTitle(), RooArgSet(x), Import(*histo, kFALSE));
+    
     // Crea un modello gaussiano per il fit
     RooRealVar mean("mean", "Mean", 1.0, 0.96, 1.06);
-    RooRealVar sigma("sigma", "Sigma", 0.01, 0.001, 0.1);
+    RooRealVar sigma("sigma", "Sigma", 0.1, 0.01, 0.5);
     RooGaussian gauss("gauss", "Gaussian PDF", x, mean, sigma);
 
     // Crea un modello esponenziale per il fondo
-    RooRealVar lambda("lambda", "Exponential Decay Parameter", -0.1, -1.0, 0.0);
+    RooRealVar lambda("lambda", "Exponential Decay Parameter", -0.1, -10.0, 10.0);
     RooExponential exp("exp", "Exponential PDF", x, lambda);
 
     // Crea un modello composto da gaussiana e esponenziale come fondo
@@ -33,14 +37,18 @@ void single_fit(TString cat, TString lable, TH1F* histo){
     RooFitResult *fitResult = model.fitTo(data, RooFit::Save());
 
     // Crea un frame per visualizzare il fit
-    RooPlot *frame = x.frame();
+    RooPlot *xframe = x.frame();
+    model.paramOn(xframe, Parameters(RooArgSet(mean,sigma,lambda,nbkg,nsig)), Layout(0.6,0.9,0.9));
     data.plotOn(frame);
-    gauss.plotOn(frame);
+    totalPDF.plotOn(xframe, Components(RooArgSet(gauss)), LineColor(kRed), LineStyle(kDashed));
+    totalPDF.plotOn(xframe, Components(RooArgSet(exp)), LineColor(kGreen), LineStyle(kDashed) );
+    totalPDF.plotOn(xframe);
 
     // Visualizza il frame
     TCanvas *canvas = new TCanvas("canvas", "Fit Result");
     frame->Draw();
     canvas->SaveAs("Fit_"+lable+"_cat_"+cat+".png");
+    delete canvas;
 }
 
 void fitDimu_OS() {
@@ -58,15 +66,15 @@ void fitDimu_OS() {
     TH1F *h_OS1 = new TH1F("h_OS1", "Dimu_OS1 Distribution", 100, 0.96, 1.06);
     TH1F *h_OS2 = new TH1F("h_OS2", "Dimu_OS1 Distribution", 100, 0.96, 1.06);
 
-    tree->Draw("dimu_OS1>>h_OS1", "isMC==0 && category==0");
-    tree->Draw("dimu_OS1>>h_OS2", "isMC==0 && category==0");
+    TString cat[3] = {"0","1","2"};
+    TString OS1_2[2] = {"1","2"};
 
-
-    single_fit(TH1F* h_OS1);
-    
-
-    // Pulisci la memoria
-    delete histogram;
+    for(int i=0; i<3;i++){
+        for(int k=0; k<2;k++){
+            TH1F *h_OS = new TH1F("h_OS"+OS1_2[k], "Dimu_OS Distribution", 200, 0.90, 1.10);    
+            tree->Draw("dimu_OS1>>h_OS"+OS1_2[k], "isMC==0 && category=="+cat[i]);
+            single_fit(cat[i], OS1_2[k] , h_OS);
+            delete h_OS;
+                
     delete file;
-    delete canvas;
 }
