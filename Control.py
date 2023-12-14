749104c4c17e8efcab4a9616010b918f08a8489d
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-14 12:40:52 +0100

    Update Control.py

--- Control.py
+++ Control.py
@@ -259,11 +259,14 @@ def fit(tree, year, lumi, era):
     fb = fsigregion_bkg.getVal()
     fb_err = fsigregion_bkg.getPropagatedError(r)
 
-    nsigevents = fs * (nSig_right.getVal() + nSig_left.getVal() + nBkg.getVal()) - fb * nBkg.getVal()
+    //nsigevents = fs * (nSig_right.getVal() + nSig_left.getVal() + nBkg.getVal()) - fb * nBkg.getVal()
     nsig_err = ROOT.TMath.Sqrt(fs_err**2 * (nSig_right.getVal() + nSig_left.getVal() + nBkg.getVal())**2 +
                                (nSig_right.getPropagatedError(r)**2 + nSig_left.getPropagatedError(r)**2 + nBkg.getPropagatedError(r)**2) * fs**2 + 
                                fb_err**2 * nBkg.getVal()**2 + nBkg.getPropagatedError(r)**2 * fb**2 )
 
+    nsigevents = fs * (nSig_right.getVal())
+    nsig_err = ROOT.TMath.Sqrt(fs_err**2 * (nSig_right.getVal())**2 + fs**2 * nSig_right.getPropagatedError(r)**2)
+    
     fsig = nsigevents / (fsigregion_model.getVal() * (nSig_right.getVal() + nSig_left.getVal() + nBkg.getVal()))
     
     # Save in pd dataframe
