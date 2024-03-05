a3f3fac43de1f07fa64677e4de197720166e5092
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-05 23:03:07 +0100

    Update DsPhiPi_sPlot.cpp

--- DsPhiPi_sPlot.cpp
+++ DsPhiPi_sPlot.cpp
@@ -237,7 +237,9 @@ void DsPhiPi_sPlot(TString name_file = "AllControl2022", TString tree_name = "Fi
     if(isMC == 0) DoSPlot(wspace);
     else DoSPlotMC(wspace);
     const TTree *tree = wspace.data("dataWithSWeights")->GetClonedTree();
-    TTree *clonedTree = tree->CloneTree();
+    
+    TTree *clonedTree = static_cast<TTree*>(const_cast<TTree*>(tree)->CloneTree());
+
     TFile *file = new TFile(name_file+"_sPlot_MC_"+Form("%d", isMC)+".root", "RECREATE");
     clonedTree->SetBranchStatus("L_nsigDp", 0);
     clonedTree->SetBranchStatus("nsigDp_sw", 0);
