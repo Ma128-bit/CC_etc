17edcf89577297930992428d671563501ed51b62
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-05 13:03:29 +0100

    Create Control_plots.py

---
+++ Control_plots.py
@@ -0,0 +1,202 @@
+import os
+import ROOT
+ROOT.gROOT.SetBatch(True)
+import subprocess
+import argparse
+import pandas as pd
+from ROOT import *
+from file_locations import *
+
+var = ["cLP", "tKink", "segmComp", "fv_nC", "d0sig", "fv_dphi3D", "fv_d3Dsig", "mindca_iso", "trkRel", "d0sig_max", "MVASoft1", "MVASoft2","Ptmu3", "fv_d3D"]
+
+invmass_SB = "(tripletMass<1.8 && tripletMass>1.70)"
+invmass_peak = "(tripletMass<2.01 && tripletMass>1.93)"
+binning_mass = "(65, 1.60, 2.02)"
+
+binning_dict = {
+    "fv_d3D": "(100,0,1.5)",
+    "Ptmu1": "(60,0,30)",
+    "Ptmu2": "(60,0,30)",
+    "Ptmu3": "(60,0,20)",
+    "Etamu1": "(30,0,2.5)",
+    "Etamu2": "(30,0,2.5)",
+    "Etamu3": "(30,0,2.5)",
+    "Pmu3": "(100,0,50)",
+    "cLP": "(60,0,20)",
+    "segmComp": "(100,-0.1,1.1)",
+    "tKink": "(50,0,50)",
+    "fv_nC": "(50,-0.1,5.1)",
+    "d0sig": "(36,-0.1,18)",
+    "d0sig_max": "(36,-0.1,30)",
+    "mindca_iso": "(40,0,0.4)",
+    "trkRel": "(40,0.05,1.1)",
+    "tripletMassReso": "(80,0,0.02)",
+    "fv_dphi3D": "(42,-0.01,0.20)",
+    "fv_d3Dsig": "(50,-0.1,80)",
+    "MVASoft1": "(50,0.2,0.8)",
+    "MVASoft2": "(50,0.2,0.8)"
+}
+
+def fit_bkg(h1):
+    x = RooRealVar("x", "2mu+1trk inv. mass (GeV)", 1.65, 2.05)
+    x.setBins(40)
+    datahist = RooDataHist("datahist", h1.GetTitle(), RooArgSet(x), RooFit.Import(h1, ROOT.kFALSE))
+
+    x.setRange("R1", 1.70, 1.80)
+    x.setRange("R2", 1.93, 2.01)
+
+    gamma = RooRealVar("#Gamma", "Gamma", -1, -2.0, -1e-2)
+    exp_bkg = RooExponential("exp_bkg", "exp_bkg", x, gamma)
+    exp_bkg.fitTo(datahist, RooFit.Range("R1"))
+    exp_bkg.createIntegral(x, RooFit.NormSet(x), RooFit.Range("R2"))
+
+
+def control_plots(file_name, year):
+    if not os.path.exists("Control_Plots"):
+        subprocess.run(["mkdir", "Control_Plots"])
+    
+    # Data ALL
+    data = TChain("FinalTree")
+    data.Add(data)
+    
+    for k in range(len(var)):
+        varname = var[k]
+        s = str(k)
+        binning = binning_dict[varname]
+        data.Draw(varname + ">>hdata_bkg" + s+ binning, "control_weight*(isMC==0 &&" + invmass_SB+")")
+        data.Draw(varname + ">>hdata_sgn" + s + binning, "control_weight*(isMC==0 &&" +invmass_peak+")")
+        hdata_bkg = TH1F(gDirectory.Get("hdata_bkg" + s))
+        hdata_sgn = TH1F(gDirectory.Get("hdata_sgn" + s))
+        
+        data.Draw(varname + ">>hMC_sgn" + s + binning, "control_weight*(isMC>0 &&" +invmass_peak+")")
+        hMC_sgn = TH1F(gDirectory.Get("hMC_sgn" + s))
+
+        c2 = TCanvas("c2", "c2", 150, 10, 990, 660)
+        pad1 = TPad("pad1", "pad1", 0, 0.3, 1, 1.0)
+        pad1.SetBottomMargin(0)
+        pad1.SetGridx()
+        pad1.Draw()
+        pad1.cd()
+        hMC_sgn.SetTitle(varname)
+        
+        # Scaling the SB distribution to the number of background events in 1.93,2.01
+        normSB = hdata_bkg.GetEntries()
+        fsigregion_bkg_val = connection_values[0]
+        nbkg_val = connection_values[1]
+        hdata_bkg.Scale(fsigregion_bkg_val * nbkg_val / normSB)
+        #print("Entries in hdata_sgn before SB subtraction:", hdata_sgn.GetEntries())
+        hdata_sgn.Add(hdata_bkg, -1)  # subtract h2 from h1: h1->Add(h2,-1)
+
+        # Rescaling
+        hdata_sgn.Scale(hMC_sgn.Integral() / hdata_sgn.Integral())
+
+        #print("Entries in hdata_sgn after SB subtraction:", hdata_sgn.GetEntries())
+        #print("Entries in hMC_sgn after rescaling:", hMC_sgn.GetEntries())
+
+        # Plot makeup
+        Y_max = max(hMC_sgn.GetMaximum(), hdata_sgn.GetMaximum())
+        Y_max = Y_max * 1.05
+        hMC_sgn.GetYaxis().SetRangeUser(0, Y_max)
+
+        hMC_sgn.GetYaxis().SetTitle("a.u.")
+        hMC_sgn.GetXaxis().SetTitle(varname)
+        hMC_sgn.GetYaxis().SetTitleSize(22)
+        hMC_sgn.GetYaxis().SetTitleFont(43)
+        hMC_sgn.GetYaxis().SetTitleOffset(1.25)
+
+        hMC_sgn.SetLineColor(kBlue)
+        hMC_sgn.SetLineWidth(3)
+        hMC_sgn.SetFillStyle(3004)
+        hMC_sgn.SetFillColor(kBlue)
+        hdata_sgn.SetLineColor(kRed)
+        hdata_sgn.SetLineWidth(3)
+        hdata_sgn.SetFillStyle(3005)
+        hdata_sgn.SetFillColor(kRed)
+
+        hMC_sgn.Draw("hist")
+        hdata_sgn.Draw("hist same")
+
+        hMC_sgn.SetStats(0)
+        x_leg_left = 0.55
+        x_leg_right = 0.90
+        y_leg_left = 0.63
+        y_leg_right = 0.90
+        if varname == "segmComp" or varname == "MVASoft1" or varname == "MVASoft2":
+            x_leg_left = 0.1
+            x_leg_right = 0.45
+        leg = TLegend(x_leg_left, y_leg_left, x_leg_right, y_leg_right)
+        leg.AddEntry(hMC_sgn, "MC DsPhiPi", "f")
+        leg.AddEntry(hdata_sgn, "data (SB subtracted)", "f")
+        leg.Draw()
+
+        # Lower plot will be in pad2
+        c2.cd()
+        pad2 = TPad("pad2", "pad2", 0, 0.05, 1, 0.3)
+        pad2.SetTopMargin(0)
+        pad2.SetBottomMargin(0.2)
+        pad2.SetGridx()
+        pad2.Draw()
+        pad2.cd()
+
+        # Define the ratio plot
+        h_x_ratio = hdata_sgn.Clone("h_x_ratio")
+        h_x_ratio.Sumw2()
+        h_x_ratio.Divide(hMC_sgn)
+        h_x_ratio.SetStats(0)
+
+        # Ratio plot settings
+        h_x_ratio.SetTitle("")  # Remove the ratio title
+        h_x_ratio.GetYaxis().SetTitle("ratio data/MC")
+        h_x_ratio.GetYaxis().SetRangeUser(-0.5, 2)
+
+        h_x_ratio.SetLineColor(kBlack)
+        h_x_ratio.GetYaxis().SetTitleSize(22)
+        h_x_ratio.GetYaxis().SetTitleFont(43)
+        h_x_ratio.GetYaxis().SetTitleOffset(1.25)
+        h_x_ratio.GetYaxis().SetLabelFont(43)
+        h_x_ratio.GetYaxis().SetLabelSize(15)
+
+        # X axis ratio plot settings
+        h_x_ratio.GetXaxis().SetTitle(varname)
+        h_x_ratio.GetXaxis().SetTitleSize(22)
+        h_x_ratio.GetXaxis().SetTitleFont(43)
+        h_x_ratio.GetXaxis().SetTitleOffset(2.0)
+        h_x_ratio.GetXaxis().SetLabelFont(43)
+        h_x_ratio.GetXaxis().SetLabelSize(15)
+
+        # Compute weighted average ratio
+        mean = 0
+        std_dev = 0
+        for c in range(1, h_x_ratio.GetNbinsX() + 1):
+            if h_x_ratio.GetBinContent(c) == 0 or h_x_ratio.GetBinError(c) == 0:
+                continue
+            mean += h_x_ratio.GetBinContent(c) / (h_x_ratio.GetBinError(c) * h_x_ratio.GetBinError(c))
+            std_dev += 1 / (h_x_ratio.GetBinError(c) * h_x_ratio.GetBinError(c))
+        mean = mean / std_dev
+        std_dev = 1 / std_dev
+
+        # Get mean value and error of ratio plot
+        #print(var[k] + " Mean:", mean)
+        #print(var[k] + " StdDev:", std_dev)
+
+        # Draw line corresponding to mean value on ratio plot
+        line = TLine()
+        h_x_ratio.Draw("ep")
+        line.SetLineWidth(2)
+        line.SetLineColor(kRed)
+        line.DrawLine(float(binning.split(',')[1]), 1, h_x_ratio.GetXaxis().GetXmax(), 1)
+        h_x_ratio.Draw("same")
+
+        c2.cd()
+        c2.Update()
+        c2.SaveAs("Control_Plots/" + varname + "_"+year+".png")
+        del c2
+        del pad2
+        del pad1
+        del line
+        h_x_ratio.Delete();
+        hdata_bkg.Delete();
+        hdata_sgn.Delete();
+        hMC_sgn.Delete(); 
+        if year == "2022":
+            hMC_sgn2.Delete();
