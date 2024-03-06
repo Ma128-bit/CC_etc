2901b924d4afb005f6021427a0955e678eb9bf69
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-06 18:59:09 +0100

    Update Best_dimuOS_cut.py

--- Best_dimuOS_cut.py
+++ Best_dimuOS_cut.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
             AMS.append(math.sqrt(2*((S+B)*math.log(1+S/B) - S)))
             
         hist = TH1F("AMScat"+str(j), "AMS cat "+ str(j), 10, 0.5, 10.5)
-        for i in range(len(AMS))
+        for i in range(len(AMS)):
             hist.SetBinContent(i+1, AMS[i])
         canvas.HaddTH1(hist, Color=j+2, SetXName="cut", SetYName="a.u.", label="Category "+str(j), FillStyle = 3004+j)
         del hist
