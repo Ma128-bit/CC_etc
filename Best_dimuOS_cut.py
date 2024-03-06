df350f2f6ee8d9b3bd03c41d1dfe710213c41cc5
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-06 18:55:00 +0100

    Update Best_dimuOS_cut.py

--- Best_dimuOS_cut.py
+++ Best_dimuOS_cut.py
@@ -7,6 +7,10 @@ from file_locations import *
 class ROOTDrawer(draw_utilities.ROOTDrawer):
     pass
 
+sigma_phi = [0.011,0.015,0.018]
+sigma_omega = [0.012,0.015,0.022]
+sigma_tau = [0.011, 0.018, 0.026]
+
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description="--plots for control plots")
     parser.add_argument("--file", type=str, help="file name")
@@ -14,3 +18,34 @@ if __name__ == "__main__":
     args = parser.parse_args()
     file = args.file
     year = args.year
+    
+    data = TChain("FinalTree")
+    data.Add(file)
+
+    canvas = ROOTDrawer(SetGridx = True)
+    
+    for j in range(3):
+        sig = []
+        bkg = []
+        AMS = []
+        for i in range(1,11):
+            s = str((j+1)*i)
+            data.Draw("tripletMass>>hbkg" +s+ "(52, 1.6, 2)", "(isMC==0 && abs(1.777 - tripletMass)>"+ str(3*sigma_tau[j]) + "&& category=="+j + "&& abs(dimu_OS1 - 1.0195)>"str((i/2)*sigma_phi[j])  + "&& abs(dimu_OS2 - 1.0195)>"str((i/2)*sigma_phi[j])  +")")
+            data.Draw("tripletMass>>hsig" +s+ "(52, 1.6, 2)", "combine_weight*(isMC>0" + "&& category=="+j + "&& abs(dimu_OS1 - 1.0195)>"str((i/2)*sigma_phi[j])  + "&& abs(dimu_OS2 - 1.0195)>"str((i/2)*sigma_phi[j])  +")")
+            h_sig = gDirectory.Get("h_sig"+s)
+            h_bkg = gDirectory.Get("h_bkg"+s)
+            S=h_sig.Integral()
+            B=h_bkg.Integral()*6*sigma_tau[j]/(0.4-6*sigma_tau[j])
+            sig.append(S)
+            bkg.append(B)
+            AMS.append(math.sqrt(2*((S+B)*math.log(1+S/B) - S)))
+            
+        hist = TH1F("AMScat"+str(j), "AMS cat "+ str(j), 10, 0.5, 10.5)
+        for i in range(len(AMS))
+            hist.SetBinContent(i+1, AMS[i])
+        canvas.HaddTH1(hist, Color=j+2, SetXName="cut", SetYName="a.u.", label="Category "+str(j), FillStyle = 3004+j)
+        del hist
+
+    canvas.MakeLegend()
+    canvas.Save("prova.png", era=int(year), extra="Preliminary")
+    
