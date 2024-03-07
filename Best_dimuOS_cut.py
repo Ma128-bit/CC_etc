ae153eacbd5cdbd80b961695057237f94a970bf2
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-07 10:05:47 +0100

    Update Best_dimuOS_cut.py

--- Best_dimuOS_cut.py
+++ Best_dimuOS_cut.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         hist = TH1F("AMScat"+str(j), "AMS cat "+ str(j), 11, -0.25, 5.25)
         for i in range(len(AMS)):
             hist.SetBinContent(i+1, AMS[i])
-        canvas.HaddTH1(hist, Color=j+2, SetXName="N. #sigma", SetYName="a.u.", label="Category "+str(j), DrawOpt = "P")
+        canvas.HaddTH1(hist, Color=j+2, SetXName="N. #sigma", SetYName="a.u.", label="Category "+str(j), DrawOpt = "P", MarkerColor=j+2)
         del hist
 
     canvas.MakeLegend()
