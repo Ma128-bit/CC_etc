49147129c430336e7026727d30718cd5f311348f
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-07 10:32:12 +0100

    Update Best_dimuOS_cut.py

--- Best_dimuOS_cut.py
+++ Best_dimuOS_cut.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         sig = []
         bkg = []
         AMS = []
-        for i in range(0,11):
+        for i in range(0,21):
             s = str((j+1)*(i+1))
             data.Draw("tripletMass>>h_bkg" +s+ "(52, 1.6, 2)", "(isMC==0 && abs(1.777 - tripletMass)>"+ str(3*sigma_tau[j]) + "&& category=="+ str(j) + "&& abs(dimu_OS1 - 0.7826)>"+str((i/2)*0.01)  + "&& abs(dimu_OS2 - 0.7826)>"+str((i/2)*0.01) + "&& abs(dimu_OS1 - 1.0195)>"+str((i/2)*0.01)  + "&& abs(dimu_OS2 - 1.0195)>"+str((i/2)*0.01) +")")
             data.Draw("tripletMass>>h_sig" +s+ "(52, 1.6, 2)", "combine_weight*(isMC>0 && abs(1.777 - tripletMass)<"+ str(3*sigma_tau[j]) + "&& category=="+ str(j) + "&& abs(dimu_OS1 - 0.7826)>"+str((i/2)*0.01)  + "&& abs(dimu_OS2 - 0.7826)>"+str((i/2)*0.01) + "&& abs(dimu_OS1 - 1.0195)>"+str((i/2)*0.01)  + "&& abs(dimu_OS2 - 1.0195)>"+str((i/2)*0.01)  +")")
@@ -41,7 +41,7 @@ if __name__ == "__main__":
             #AMS.append(S/B)
             AMS.append(math.sqrt(2*((S+B)*math.log(1+S/B) - S)))
             
-        hist = TH1F("AMScat"+str(j), "AMS cat "+ str(j), 11, -0.25, 5.25)
+        hist = TH1F("AMScat"+str(j), "AMS cat "+ str(j), 21, -0.25, 10.25)
         for i in range(len(AMS)):
             hist.SetBinContent(i+1, AMS[i])
         canvas.HaddTH1(hist, Color=j+2, SetXName="cut [10 MeV]", SetYName="a.u.", label="Category "+str(j), DrawOpt = "P", MarkerColor=j+2)
