28a61a098d04e32107a17fb5a08c872b62ac3e0c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-04-03 07:54:57 +0200

    Update MVA_predictions.py

--- MVA_predictions.py
+++ MVA_predictions.py
@@ -39,7 +39,7 @@ def predict(data, index, model):
     predictions = model.predict_proba(X)
     print(predictions)
     data["privateMVAID_mu"+str(index)] = predictionsID
-    data["privateMVA_mu"+str(index)] = predictions[1]
+    data["privateMVA_mu"+str(index)] = predictions[:,0]
     return data
 
 
