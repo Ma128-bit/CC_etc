5b2da3e7fd2a3bc71bc3d73838e0cd52867d751f
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-04-18 10:36:22 +0200

    Update MVA_predictions.py

--- MVA_predictions.py
+++ MVA_predictions.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from ROOT import RDF
 from sklearn.ensemble import HistGradientBoostingClassifier
-
+"""
 branches_MVA =[
     "Ptmu", "Etamu", "Vx", "Vy", "Vz", "cQ_uS_", "cQ_tK_", "cQ_gK_", "cQ_tRChi2_",
     "cQ_sRChi2_", "cQ_Chi2LP_", "cQ_Chi2LM_", "cQ_lD_", "cQ_gDEP_", "cQ_tM_", "cQ_gTP_", 
@@ -14,7 +14,25 @@ branches_MVA =[
     "innerTrk_ValidFraction_", "innerTrk_highPurity_", 
     "innerTrk_normChi2_", "outerTrk_normChi2_", "outerTrk_muStValidHits_"   
 ]
-
+"""
+branches_MVA = [
+    'cQ_Chi2LP_',
+    'cQ_tK_',
+    'nValidTrackerHits',
+    'nValidPixelHits',
+    'innerTrk_ValidFraction_',
+    'cQ_Chi2LM_',
+    'cQ_sRChi2_',
+    'cQ_tRChi2_',
+    'cQ_gDEP_',
+    'cQ_gK_',
+    'trkLayersWMeas',
+    'nStMu',
+    'segmComp_',
+    'GLnormChi2_mu',
+    'innerTrk_normChi2_',
+    'outerTrk_normChi2_'
+]
 def load_data(file_name):
     """Load ROOT data and turn tree into a pd dataframe"""
     print("Loading data from", file_name)
@@ -46,9 +64,9 @@ def predict(data, index, model):
 
 
 if __name__ == "__main__":
-    #file = "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/Era_C_tau3mu.root"
+    file = "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/Era_C_tau3mu.root"
     #file = "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/MC_Ds_preE.root"
-    file = "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/MC_Bp_preE.root"
+    #file = "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/MC_Bp_preE.root"
     model = joblib.load('privateMVA.pkl')
     data = load_data(file)
     print(len(data))
