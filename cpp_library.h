1d6a11494b445ddd6426528dc2811bd6692f5e82
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-04-22 16:49:02 +0200

    Update cpp_library.h

--- cpp_library.h
+++ cpp_library.h
@@ -15,13 +15,16 @@
 #include <TString.h>
 #include <TH2F.h>
 
-double xsection_Bp_preE = 3.5e+9, xsection_Bp_postE = 3.5e+9;
-double xsection_Ds_preE = 9.8e+9, xsection_Ds_postE = 9.8e+9;
-double xsection_B0_preE = 3.5e+9, xsection_B0_postE = 3.5e+9;
-double xsection_DsPhiPi_preE = 1.1e+10, xsection_DsPhiPi_postE = 1.1e+10;
+double xsection_Bp = 3.5e+9;
+double xsection_Ds = 9.8e+9;
+double xsection_B0 = 3.5e+9;
+double xsection_DsPhiPi = 1.1e+10;
 
 double lumi_tau3mu_preE = 7.848, lumi_tau3mu_postE = 26.332;
 double lumi_control_preE = 0.399, lumi_control_postE = 1.317;
+
+double lumi_tau3mu_preBPix = 7.848, lumi_tau3mu_postBPix = 26.332;
+double lumi_control_preBPix = 0.399, lumi_control_postBPix = 1.317;
 
 double BR_DsPhiPi = 4.5e-2, BR_Phi2mu = 2.85e-4;
 double BR_Dstau = 5.48e-2, BR_Bptau = 3.33e-2, BR_B0tau = 3.35e-2;
@@ -32,8 +35,16 @@ double N_Ds_preE = 2053049.0, N_Ds_postE = 7121988.0;
 double N_B0_preE = 808162.0, N_B0_postE = 2687450.0;
 double N_DsPhiPi_preE = 290495.0, N_DsPhiPi_postE = 1199193.0;
 
+double N_Bp_preBPix = 510862.0, N_Bp_postBPix = 1623692.0;
+double N_Ds_preBPix = 2053049.0, N_Ds_postBPix = 7121988.0;
+double N_B0_preBPix = 808162.0, N_B0_postBPix = 2687450.0;
+double N_DsPhiPi_preBPix = 290495.0, N_DsPhiPi_postBPix = 1199193.0;
+
 double weight_CC_preE = 0.71, weight_CC_postE = 0.82;
 double weight_CC_preE_err = 0.04, weight_CC_postE_err = 0.08;
+
+double weight_CC_preBPix = 0.71, weight_CC_postBPix = 0.82;
+double weight_CC_preBPix_err = 0.04, weight_CC_postBPix_err = 0.08;
 
 struct add_index{
     int i;
@@ -50,11 +61,15 @@ double add_vrtx_proba(double fv_nc){
 double add_weight_CC(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
     if(id.Contains("_preE") && !(id.Contains("DsPhiPi"))) return weight_CC_preE;
     if(id.Contains("_postE") && !(id.Contains("DsPhiPi"))) return weight_CC_postE;
+    if(id.Contains("_preBPix") && !(id.Contains("DsPhiPi"))) return weight_CC_preBPix;
+    if(id.Contains("_postBPix") && !(id.Contains("DsPhiPi"))) return weight_CC_postBPix;
     else return 1;
 }
 double add_weight_CC_err(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
     if(id.Contains("_preE") && !(id.Contains("DsPhiPi"))) return weight_CC_preE_err;
     if(id.Contains("_postE") && !(id.Contains("DsPhiPi"))) return weight_CC_postE_err;
+    if(id.Contains("_preBPix") && !(id.Contains("DsPhiPi"))) return weight_CC_preBPix_err;
+    if(id.Contains("_postBPix") && !(id.Contains("DsPhiPi"))) return weight_CC_postBPix_err;
     else return 0;
 }
 TString add_ID(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
@@ -67,18 +82,38 @@ TString add_ID(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
     if(id.Contains("MC_Bp_postE.root")) return "Bp_postE"; //isMC=2
     if(id.Contains("MC_DsPhiPi_preE.root")) return "DsPhiPi_preE";
     if(id.Contains("MC_DsPhiPi_postE.root")) return "DsPhiPi_postE";
+    
+    if(id.Contains("MC_Ds_preBPix.root")) return "Ds_preBPix"; //isMC=1
+    if(id.Contains("MC_Ds_postBPix.root")) return "Ds_postBPix"; //isMC=1
+    if(id.Contains("MC_B0_preBPix.root")) return "B0_preBPix"; //isMC=3
+    if(id.Contains("MC_B0_postBPix.root")) return "B0_postBPix"; //isMC=3
+    if(id.Contains("MC_Bp_preBPix.root")) return "Bp_preBPix"; //isMC=2
+    if(id.Contains("MC_Bp_postBPix.root")) return "Bp_postBPix"; //isMC=2
+    if(id.Contains("MC_DsPhiPi_preBPix.root")) return "DsPhiPi_preBPix";
+    if(id.Contains("MC_DsPhiPi_postBPix.root")) return "DsPhiPi_postBPix";
+    
     if(id.Contains("Era_")) return "Data";
     else return "None";
 }
 double add_weight(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
-    if(id.Contains("MC_Ds_preE.root")) return (xsection_Ds_preE*lumi_tau3mu_preE*BR_tau3mu*BR_Dstau/N_Ds_preE);
-    if(id.Contains("MC_Ds_postE.root")) return (xsection_Ds_postE*lumi_tau3mu_postE*BR_tau3mu*BR_Dstau/N_Ds_postE);
-    if(id.Contains("MC_B0_preE.root")) return (xsection_B0_preE*lumi_tau3mu_preE*BR_tau3mu*BR_B0tau/N_B0_preE);
-    if(id.Contains("MC_B0_postE.root")) return (xsection_B0_postE*lumi_tau3mu_postE*BR_tau3mu*BR_B0tau/N_B0_postE);
-    if(id.Contains("MC_Bp_preE.root")) return (xsection_Bp_preE*lumi_tau3mu_preE*BR_tau3mu*BR_Bptau/N_Bp_preE);
-    if(id.Contains("MC_Bp_postE.root")) return (xsection_Bp_postE*lumi_tau3mu_postE*BR_tau3mu*BR_Bptau/N_Bp_postE);
-    if(id.Contains("MC_DsPhiPi_preE.root")) return (xsection_DsPhiPi_preE*lumi_control_preE*BR_Phi2mu*BR_DsPhiPi/N_DsPhiPi_preE);
-    if(id.Contains("MC_DsPhiPi_postE.root")) return (xsection_DsPhiPi_postE*lumi_control_postE*BR_Phi2mu*BR_DsPhiPi/N_DsPhiPi_postE);
+    if(id.Contains("MC_Ds_preE.root")) return (xsection_Ds*lumi_tau3mu_preE*BR_tau3mu*BR_Dstau/N_Ds_preE);
+    if(id.Contains("MC_Ds_postE.root")) return (xsection_Ds*lumi_tau3mu_postE*BR_tau3mu*BR_Dstau/N_Ds_postE);
+    if(id.Contains("MC_B0_preE.root")) return (xsection_B0*lumi_tau3mu_preE*BR_tau3mu*BR_B0tau/N_B0_preE);
+    if(id.Contains("MC_B0_postE.root")) return (xsection_B0*lumi_tau3mu_postE*BR_tau3mu*BR_B0tau/N_B0_postE);
+    if(id.Contains("MC_Bp_preE.root")) return (xsection_Bp*lumi_tau3mu_preE*BR_tau3mu*BR_Bptau/N_Bp_preE);
+    if(id.Contains("MC_Bp_postE.root")) return (xsection_Bp*lumi_tau3mu_postE*BR_tau3mu*BR_Bptau/N_Bp_postE);
+    if(id.Contains("MC_DsPhiPi_preE.root")) return (xsection_DsPhiPi*lumi_control_preE*BR_Phi2mu*BR_DsPhiPi/N_DsPhiPi_preE);
+    if(id.Contains("MC_DsPhiPi_postE.root")) return (xsection_DsPhiPi*lumi_control_postE*BR_Phi2mu*BR_DsPhiPi/N_DsPhiPi_postE);
+
+    if(id.Contains("MC_Ds_preBPix.root")) return (xsection_Ds*lumi_tau3mu_preBPix*BR_tau3mu*BR_Dstau/N_Ds_preBPix);
+    if(id.Contains("MC_Ds_postBPix.root")) return (xsection_Ds*lumi_tau3mu_postBPix*BR_tau3mu*BR_Dstau/N_Ds_postBPix);
+    if(id.Contains("MC_B0_preBPix.root")) return (xsection_B0*lumi_tau3mu_preBPix*BR_tau3mu*BR_B0tau/N_B0_preBPix);
+    if(id.Contains("MC_B0_postBPix.root")) return (xsection_B0*lumi_tau3mu_postBPix*BR_tau3mu*BR_B0tau/N_B0_postBPix);
+    if(id.Contains("MC_Bp_preBPix.root")) return (xsection_Bp*lumi_tau3mu_preBPix*BR_tau3mu*BR_Bptau/N_Bp_preBPix);
+    if(id.Contains("MC_Bp_postBPix.root")) return (xsection_Bp*lumi_tau3mu_postBPix*BR_tau3mu*BR_Bptau/N_Bp_postBPix);
+    if(id.Contains("MC_DsPhiPi_preBPix.root")) return (xsection_DsPhiPi*lumi_control_preBPix*BR_Phi2mu*BR_DsPhiPi/N_DsPhiPi_preBPix);
+    if(id.Contains("MC_DsPhiPi_postBPix.root")) return (xsection_DsPhiPi*lumi_control_postBPix*BR_Phi2mu*BR_DsPhiPi/N_DsPhiPi_postBPix);
+    
     if(id.Contains("Era_")) return 1;
     else return -1;
 }
@@ -89,6 +124,14 @@ double add_weight_MC(unsigned int slot, const ROOT::RDF::RSampleInfo &id){
     if(id.Contains("MC_B0_postE.root")) return ((N_Bp_postE/N_B0_postE)*(BR_B0tau/BR_Bptau));
     if(id.Contains("MC_Bp_preE.root")) return ((N_Bp_preE/N_Bp_preE)*(BR_Bptau/BR_Bptau));
     if(id.Contains("MC_Bp_postE.root")) return ((N_Bp_postE/N_Bp_postE)*(BR_Bptau/BR_Bptau));
+
+    if(id.Contains("MC_Ds_preBPix.root")) return ((N_Bp_preBPix/N_Ds_preBPix)*(BR_Dstau/BR_Bptau));
+    if(id.Contains("MC_Ds_postBPix.root")) return ((N_Bp_postBPix/N_Ds_postBPix)*(BR_Dstau/BR_Bptau));
+    if(id.Contains("MC_B0_preBPix.root")) return ((N_Bp_preBPix/N_B0_preBPix)*(BR_B0tau/BR_Bptau));
+    if(id.Contains("MC_B0_postBPix.root")) return ((N_Bp_postBPix/N_B0_postBPix)*(BR_B0tau/BR_Bptau));
+    if(id.Contains("MC_Bp_preBPix.root")) return ((N_Bp_preBPix/N_Bp_preBPix)*(BR_Bptau/BR_Bptau));
+    if(id.Contains("MC_Bp_postBPix.root")) return ((N_Bp_postBPix/N_Bp_postBPix)*(BR_Bptau/BR_Bptau));
+    
     if(id.Contains("Era_") || id.Contains("DsPhiPi")) return 1;
     else return -1;
 }
