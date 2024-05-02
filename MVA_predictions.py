97c80ae2f61d25233bfa845c57a096ab71f26751
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-05-02 12:25:00 +0200

    Update MVA_predictions.py

--- MVA_predictions.py
+++ MVA_predictions.py
@@ -74,7 +74,7 @@ def predict(data, index, model):
 if __name__ == "__main__":
     files = ["/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/Era_F_tau3mu.root",
              "/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/Analysis/JobAdd_perEra/MC_Ds_preE.root"]
-    model = joblib.load('Tau3MuMVA_PtEtaReweight_NoVxVyVz.pkl')
+    model = joblib.load('Tau3MuMVA_PtEtaReweight_NoPtEtaVxVyVz.pkl')
     #model = joblib.load('privateMVA.pkl')
     data = load_data(files)
     """
